d84e0c3012581c58b76cf89e900b0ffe719f5b17
Eduardo J.S <103137675+Eduardo-J-S@users.noreply.github.com>
2022-07-04 23:59:21 -0300

    Update telaCompras.py

--- pacotes/telaCompras.py
+++ pacotes/telaCompras.py
@@ -1,10 +1,11 @@
 from tkinter.ttk import *
-
 from tkinter import *
 from tkinter import messagebox
-import mysql.connector
 import pycep_correios
 from pacotes import criarBanco
+import datetime
+
+
 
 
 # --------------------------------------------- cores --------------------------------------------
@@ -16,6 +17,222 @@ cor4 = '#403d3d'   # letra / letters
 cor5 = '#e9edf5' # sky blue
 cor6 = '#ef5350' #vermelho
 cor7 = '#191970' #MidnightBlue
+cor8 = '#ffff40' #amarelo claro
+
+
+def se_pag_debito():
+    for w in frame_topo_t2.winfo_children():
+            w.destroy()
+
+    for w in frame_baixo_t2.winfo_children():
+            w.destroy()
+
+    for w in frame_baixo_total_pagameto.winfo_children():
+            w.destroy()
+
+    for w in frame_baixo_baixo_t2.winfo_children():
+            w.destroy()
+    
+    listra = '----------------------------------------------------------------------------------'
+
+    #------------------- label topo ----------------------------------------------------------
+    label_top_comprovante_deb = Label(frame_topo_t2, text='Comprovante de compra', font=('Arialblack 20 bold'), bg=cor3, fg=cor1)
+    label_top_comprovante_deb.grid(row=0, column=0, padx=100, pady=10)
+
+    #---------------- frame pix comprovante --------------------------------------------------
+    frame_deb = Frame(frame_baixo_t2, width=450, height=260, bg=cor8, relief='flat')
+    frame_deb.place(x=20, y=58)
+
+
+    label_deb_lista_cima = Label(frame_deb, text=listra, font=('Arialblack 12 bold'), bg=cor8, fg=cor0, relief='flat')
+    label_deb_lista_cima.place(x=10, y=2)
+
+    label_deb_confe = Label(frame_deb, text='CONFERENCIA DE COMPRA', font=('Arialblack 10 bold'), bg=cor8, fg=cor0, relief='flat')
+    label_deb_confe.place(x=120, y=19)
+   
+    label_deb_lista_baixo = Label(frame_deb, text=listra, font=('Arialblack 12 bold'), bg=cor8, fg=cor0, relief='flat')
+    label_deb_lista_baixo.place(x=10, y=35)
+
+    label_deb_produt = Label(frame_deb, text='Nome:', font=('Arialblack 10 bold'), bg=cor8, fg=cor0, relief='flat')
+    label_deb_produt.place(x=15, y=60)
+    
+    label_ded_produto = Label(frame_deb, text=nome_var, font=('Arialblack 10 bold'), bg=cor8, fg=cor0, relief='flat')
+    label_ded_produto.place(x=61, y=60)
+
+    label_deb_valor = Label(frame_deb, text='CPF:', font=('Arialblack 10 bold'), bg=cor8, fg=cor0, relief='flat')
+    label_deb_valor.place(x=15, y=80)
+
+    label_deb_val = Label(frame_deb, text=cpf_var, font=('Arialblack 10 bold'), bg=cor8, fg=cor0, relief='flat')
+    label_deb_val.place(x=50, y=80)
+
+    label_deb_datahora = Label(frame_deb, text='Produto:', font=('Arialblack 10 bold'), bg=cor8, fg=cor0, relief='flat')
+    label_deb_datahora.place(x=15, y=100)
+
+    label_deb_dataHora = Label(frame_deb, text=produto_select, font=('Arialblack 10 bold'), bg=cor8, fg=cor0, relief='flat')
+    label_deb_dataHora.place(x=75, y=100)
+
+    label_deb_nome = Label(frame_deb, text='Total:', font=('Arialblack 10 bold'), bg=cor8, fg=cor0, relief='flat')
+    label_deb_nome.place(x=15, y=120)
+
+    label_deb_Nome = Label(frame_deb, text=preco_quant, font=('Arialblack 10 bold'), bg=cor8, fg=cor0, relief='flat')
+    label_deb_Nome.place(x=53, y=120)
+
+    label_deb_cpf = Label(frame_deb, text='Data e hora:', font=('Arialblack 10 bold'), bg=cor8, fg=cor0, relief='flat')
+    label_deb_cpf.place(x=15, y=140)
+
+    label_deb_CPF = Label(frame_deb, text=datetime.datetime.now(), font=('Arialblack 10 bold'), bg=cor8, fg=cor0, relief='flat')
+    label_deb_CPF.place(x=95, y=140)
+
+    label_deb_lista_final = Label(frame_deb, text=listra, font=('Arialblack 12 bold'), bg=cor8, fg=cor0, relief='flat')
+    label_deb_lista_final.place(x=10, y=170)
+
+    #----------------------- botao voltar -------------------------------------------------------------------------------------------
+    botao_voltar_pix = Button(frame_baixo_total_pagameto, text='Voltar', command=cancelar_compra, font=('Arial 12'), bg=cor2, fg=cor0, overrelief='ridge', relief='raised', height=1, width=9)
+    botao_voltar_pix.place(x=320, y=10)
+
+
+def se_pag_pix():
+    for w in frame_topo_t2.winfo_children():
+            w.destroy()
+
+    for w in frame_baixo_t2.winfo_children():
+            w.destroy()
+
+    for w in frame_baixo_total_pagameto.winfo_children():
+            w.destroy()
+
+    for w in frame_baixo_baixo_t2.winfo_children():
+            w.destroy()
+
+    pix_cop_cola = '''00020126580014br.gov.bcb.pix0136f02eb187-570d-44b5-b4
+    ba-0f99e5a44b505204000053039865802BR5921
+    Eduardo Jose Da Silva6009Sao Paulo62070503***630470A7'''
+
+
+    #------------------- label topo ----------------------------------------------------------
+    label_top_comprovante = Label(frame_topo_t2, text='Comprovante de compra', font=('Arialblack 20 bold'), bg=cor3, fg=cor1)
+    label_top_comprovante.grid(row=0, column=0, padx=100, pady=10)
+
+
+    #---------------- frame pix comprovante --------------------------------------------------
+    frame_pix = Frame(frame_baixo_t2, width=450, height=260, bg=cor8, relief='flat')
+    frame_pix.place(x=20, y=58)
+
+    label_pix_chave = Label(frame_pix, text=pix_cop_cola, font=('Arialblack 10 bold'), bg=cor1, fg=cor0, relief='solid')
+    label_pix_chave.place(x=30, y=30)
+    
+    def copiar():
+        copiando = pix_cop_cola
+        frame_pix.clipboard_clear()
+        frame_pix.clipboard_append(copiando)
+        messagebox.showinfo('Copiado', 'Chave copiada com sucesso')
+
+    #----------------------- botao copiar -------------------------------------------
+    botao_pix = Button(frame_pix, text='Copiar', command=copiar, font=('Arial 12'), bg=cor2, fg=cor0, overrelief='ridge', relief='raised', height=1, width=9)
+    botao_pix.place(x=170, y=100)
+
+    #----------------------- botao voltar -------------------------------------------------------------------------------------------
+    botao_voltar_pix = Button(frame_baixo_total_pagameto, text='Voltar', command=cancelar_compra, font=('Arial 12'), bg=cor2, fg=cor0, overrelief='ridge', relief='raised', height=1, width=9)
+    botao_voltar_pix.place(x=320, y=10)
+
+
+
+
+def cancelar_compra():
+    segundaTela_compras.destroy()
+    first_tela_compras()
+
+
+def selecionar():
+    opcao_de_pagamento = pagamento.get()
+    print(opcao_de_pagamento)
+
+    opc_pagar = ['Crédito', 'Débito', 'PIX']
+
+    if opcao_de_pagamento == opc_pagar[0]:
+        for w in frame_baixo_baixo_t2.winfo_children():
+            w.destroy()
+        #---------------- labels do cartao de credito --------------------------------------------
+        label_cred_numero = Label(frame_baixo_baixo_t2, text='Número do cartão: ', font=('Arialblack 11 bold'), bg=cor1, fg=cor0)
+        label_cred_numero.place(x=15, y=5)
+        
+        label_cred_cvv = Label(frame_baixo_baixo_t2, text='CVV: ', font=('Arialblack 11 bold'), bg=cor1, fg=cor0)
+        label_cred_cvv.place(x=15, y=35)
+
+        label_cred_cpf = Label(frame_baixo_baixo_t2, text='CPF titular: ', font=('Arialblack 11 bold'), bg=cor1, fg=cor0)
+        label_cred_cpf.place(x=200, y=35)
+
+        label_cred_vencimento = Label(frame_baixo_baixo_t2, text='Vencimento do cartão: ', font=('Arialblack 11 bold'), bg=cor1, fg=cor0)
+        label_cred_vencimento.place(x=15, y=65)
+
+        #------------------------- criando as entradas do cartao de credito ----------------------------------------------
+        entre_cred_numero = Entry(frame_baixo_baixo_t2, width=20, relief='solid')
+        entre_cred_numero.place(x=157, y=7)
+
+        entre_cred_cvv = Entry(frame_baixo_baixo_t2, width=15, relief='solid')
+        entre_cred_cvv.place(x=59, y=38)
+
+        entre_cred_cpf = Entry(frame_baixo_baixo_t2, width=15, relief='solid')
+        entre_cred_cpf.place(x=292, y=38)
+
+        entre_cred_vencimento = Entry(frame_baixo_baixo_t2, width=15, relief='solid')
+        entre_cred_vencimento.place(x=184, y=68)
+
+        #--------------------- criando combobox do cartao de credito ---------------------------------------------
+        parecelamento = Combobox(frame_baixo_baixo_t2, width=15)
+        parecelamento['values'] = ('1x', '2x', '3x', '4x', '5x', '6x', '7x', '8x', '9x', '10x', '11x', '12x')
+        parecelamento.current(0)
+        parecelamento.place(x=15, y=95)
+
+        #-------------------- criando botao confirmar comprar -------------------------------------------------------
+        botao_selcionar_opc = Button(frame_baixo_baixo_t2, text='Confirmar compra ', font=('Arial 13 bold'), bg=cor2, fg=cor0, overrelief='ridge', relief='raised', height=1, width=15)
+        botao_selcionar_opc.place(x=320, y=95)
+
+
+    if opcao_de_pagamento == opc_pagar[1]:
+        for w in frame_baixo_baixo_t2.winfo_children():
+            w.destroy()
+
+
+        #---------------- labels do cartao de debito --------------------------------------------
+        label_deb_numero = Label(frame_baixo_baixo_t2, text='Número do cartão: ', font=('Arialblack 11 bold'), bg=cor1, fg=cor0)
+        label_deb_numero.place(x=15, y=5)
+
+        label_deb_cpf = Label(frame_baixo_baixo_t2, text='CPF titular: ', font=('Arialblack 11 bold'), bg=cor1, fg=cor0)
+        label_deb_cpf.place(x=15, y=35)
+
+        label_deb_vencimento = Label(frame_baixo_baixo_t2, text='Vencimento do cartão: ', font=('Arialblack 11 bold'), bg=cor1, fg=cor0)
+        label_deb_vencimento.place(x=15, y=65)
+
+        #------------------ criando entradas para o cartao de debito --------------------------------
+        global entre_deb_numero
+        global entre_deb_cpf
+
+        entre_deb_numero = Entry(frame_baixo_baixo_t2, width=20, relief='solid')
+        entre_deb_numero.place(x=157, y=7)
+
+        entre_deb_cpf = Entry(frame_baixo_baixo_t2, width=15, relief='solid')
+        entre_deb_cpf.place(x=108, y=38)
+
+        entre_deb_vencimento = Entry(frame_baixo_baixo_t2, width=15, relief='solid')
+        entre_deb_vencimento.place(x=184, y=68)
+
+        #-------------------- criando botao confirmar comprar -------------------------------------------------------
+        botao_selcionar_opc = Button(frame_baixo_baixo_t2, command=se_pag_debito, text='Confirmar compra ', font=('Arial 13 bold'), bg=cor2, fg=cor0, overrelief='ridge', relief='raised', height=1, width=15)
+        botao_selcionar_opc.place(x=320, y=95)
+
+    if opcao_de_pagamento == opc_pagar[2]:
+        for w in frame_baixo_baixo_t2.winfo_children():
+            w.destroy()
+        texto = '''Clique no botão confirmar compra para ser
+        gerado um pix copia e cola e efetuar o pagamento!'''
+        label_mensagem_pix = Label(frame_baixo_baixo_t2, text=texto, font=('Arialblack 10 bold'), bg=cor1, fg=cor0)
+        label_mensagem_pix.place(x=10, y=35)
+
+        #-------------------- criando botao confirmar comprar -------------------------------------------------------
+        botao_selcionar_opc = Button(frame_baixo_baixo_t2, command=se_pag_pix, text='Confirmar compra ', font=('Arial 13 bold'), bg=cor2, fg=cor0, overrelief='ridge', relief='raised', height=1, width=15)
+        botao_selcionar_opc.place(x=320, y=95)
+        
 
 
 
@@ -28,33 +245,44 @@ def confirmar():
     global uf_var
     global telefone_var
     global preco_quant
+    global produto_select
+
+    try:
+
+        nome_var = entre_nome_compra.get()
+        cpf_var = entre_cpf_compra.get()
+        endereco_var = entre_endereco_compra.get()
+        bairro_var = entre_bairro_compra.get()
+        cidade_var = entre_cidade_compra.get()
+        uf_var = entre_uf_compra.get()
+        telefone_var = entre_telefone_compra.get()
 
 
-    nome_var = entre_nome_compra.get()
-    cpf_var = entre_cpf_compra.get()
-    endereco_var = entre_endereco_compra.get()
-    bairro_var = entre_bairro_compra.get()
-    cidade_var = entre_cidade_compra.get()
-    uf_var = entre_uf_compra.get()
-    telefone_var = entre_telefone_compra.get()
+        quantidade = spin_qnt.get()
+        print(quantidade)
+        produto_select = comprar.get()
+        print(produto_select)
 
+        if nome_var == '' or cpf_var == '' or endereco_var == '' or bairro_var == '' or cidade_var == '' or uf_var == '' or telefone_var == '':
+            messagebox.showerror('ERRO', 'Campos não estão preenchidos') 
 
+        else:
 
-    quantidade = spin_qnt.get()
-    print(quantidade)
-    produto_select = comprar.get()
-    print(produto_select)
+            lista_produtos = criarBanco.visu_info()
+            produto = []
 
-    lista_produtos = criarBanco.visu_info()
-    produto = []
+            for linha in lista_produtos:
+                if produto_select in linha:
+                    produto.append(linha)
+            print(produto)
+            preco_quant = int(produto[0][3]) * int(quantidade)
+            print(f'{int(produto[0][3]) * int(quantidade)}')
 
-    for linha in lista_produtos:
-        if produto_select in linha:
-            produto.append(linha)
-    print(produto)
-    preco_quant = int(produto[0][3]) * int(quantidade)
-    print(f'{int(produto[0][3]) * int(quantidade)}')
-    second_tela_compras()
+            primeiraTela_compras.destroy()
+            second_tela_compras()
+
+    except:
+        messagebox.showerror('ERRO', 'Algum campo com erro!')    
 
 
 def cep():
@@ -67,7 +295,7 @@ def cep():
         dados_cep = pycep_correios.get_address_from_cep(cep)
         entre_cidade_compra.insert('end', dados_cep['cidade'])
         entre_bairro_compra.insert('end', dados_cep['bairro'])
-        endereco_var = entre_endereco_compra.insert('end', dados_cep['logradouro'])
+        entre_endereco_compra.insert('end', dados_cep['logradouro'])
         entre_uf_compra.insert('end', dados_cep['uf'])
     except:
         messagebox.showerror('ERRO', 'CEP inexistente')
@@ -87,6 +315,7 @@ def first_tela_compras():
     global entre_telefone_compra
     global entre_cpf_compra
     global pagamento
+    global primeiraTela_compras
     
 
 
@@ -177,6 +406,7 @@ def first_tela_compras():
     botao_cep_compra = Button(frame_baixo, command=cep, text='CEP ', font=('Arial 10 bold'), bg=cor2, fg=cor5, overrelief='ridge', relief='raised')
     botao_cep_compra.place(x=300, y=100)
     
+    #-------------- criando botao confirmar -------------------------------------------
     botao_confirmar_pedido = Button(frame_baixo, command=confirmar, text='Confirmar ', font=('Arial 13 bold'), bg=cor2, fg=cor0, overrelief='ridge', relief='raised', height=1)
     botao_confirmar_pedido.place(x=15, y=350)
 
@@ -184,6 +414,11 @@ def first_tela_compras():
     primeiraTela_compras.mainloop()
 
 def second_tela_compras():
+    global frame_baixo_baixo_t2
+    global segundaTela_compras
+    global frame_topo_t2
+    global frame_baixo_t2
+    global frame_baixo_total_pagameto
 
     #----------------- Criando tela 2 área de compras ---------------------------------------------------
     segundaTela_compras = Tk()
@@ -196,8 +431,14 @@ def second_tela_compras():
     frame_topo_t2 = Frame(segundaTela_compras, width=500, height=65, bg=cor3, relief='flat')
     frame_topo_t2.grid(row=0, column=0)
     
-    frame_baixo_t2 = Frame(segundaTela_compras, width=500, height=435, bg=cor1, relief='flat')
+    frame_baixo_t2 = Frame(segundaTela_compras, width=500, height=260, bg=cor1, relief='flat')
     frame_baixo_t2.place(x=0, y=58)
+
+    frame_baixo_baixo_t2 = Frame(segundaTela_compras, width=500, height=130, bg=cor1, relief='flat')
+    frame_baixo_baixo_t2.place(x=0, y=318)
+
+    frame_baixo_total_pagameto = Frame(segundaTela_compras, width=500, height=54, bg=cor1, relief='flat')
+    frame_baixo_total_pagameto.place(x=0, y=448)
 
     #-------------- Criando labels ------------------------------------------------------------------------
     label_top_conf_pedido = Label(frame_topo_t2, text='Confirmação do pedido', font=('Arialblack 20 bold'), bg=cor3, fg=cor1)
@@ -248,25 +489,26 @@ def second_tela_compras():
     label_opc_pagamento = Label(frame_baixo_t2, text='Opção de pagamento: ', font=('Arialblack 11 bold'), bg=cor1, fg=cor0)
     label_opc_pagamento.place(x=15, y=230)
 
-    label_total_pagamento = Label(frame_baixo_t2, text='Pegamento total: ', font=('Arialblack 11 bold'), bg=cor1, fg=cor0)
-    label_total_pagamento.place(x=15, y=400)
+    label_total_pagamento = Label(frame_baixo_total_pagameto, text='Pegamento total: ', font=('Arialblack 11 bold'), bg=cor1, fg=cor0)
+    label_total_pagamento.place(x=15, y=10)
     
-    label_total = Label(frame_baixo_t2, text=preco_quant, font=('Arialblack 11 bold'), bg=cor1, fg=cor0)
-    label_total.place(x=139, y=400)
+    label_total = Label(frame_baixo_total_pagameto, text=preco_quant, font=('Arialblack 11 bold'), bg=cor1, fg=cor0)
+    label_total.place(x=139, y=10)
 
     #------------------ criando combobox ---------------------------------
+    global pagamento
     pagamento = Combobox(frame_baixo_t2, width=15)
-    pagamento['values'] = ('Crédito', 'Débito', 'Boleto')
+    pagamento['values'] = ('Crédito', 'Débito', 'PIX')
     pagamento.current(0)
     pagamento.place(x=185, y=232)
 
-    def selecionar():
-        pagamento_cred = pagamento.get()
-        print(pagamento_cred)
 
     #------------------- criando botao opc comprar --------------------------------------------
     botao_selcionar_opc = Button(frame_baixo_t2, command=selecionar, text='Selecionar ', font=('Arial 13 bold'), bg=cor2, fg=cor0, overrelief='ridge', relief='raised', height=1, width=10)
-    botao_selcionar_opc.place(x=320, y=227)
+    botao_selcionar_opc.place(x=320, y=225)
 
+    #-------------------- criando botao cancelar comprar -------------------------------------------------------
+    botao_selcionar_opc = Button(frame_baixo_total_pagameto, command=cancelar_compra, text='Cancelar compra ', font=('Arial 13 bold'), bg=cor6, fg=cor0, overrelief='ridge', relief='raised', height=1, width=15)
+    botao_selcionar_opc.place(x=320, y=10)
 
     segundaTela_compras.mainloop()
